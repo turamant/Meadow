607029f7de8d0716150741c8c12139eb0e97d50e
askvart <tur1amant@gmail.com>
2023-11-25 13:55:39 +0300

    Перенос fortuneCookies в модуль Lib

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // const expressHandlebars = require('express-handlebars');
 const { engine: expressHandlebars } = require('express-handlebars')
+const fortune = require('./lib/fortune');
 
 const app = express();
 
@@ -17,18 +18,8 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res)=> res.render('home'));
 
-
-fortunes = [
-    "Победи свои страхи , или они победят тебя ." ,
-    "Рекам нужны истоки ." ,
-    "Не бойся неведомого ." ,
-    "Тебя ждет приятный сюрприз ." ,
-    "Будь проще везде , где только можно ." ,
-]
-
 app.get('/about',(req, res)=>{
-    const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune: fortune.getFortune()});
 })
 
 // Пользовательская страница 404
